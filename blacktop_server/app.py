302b6595f3aa6913c8b9bd05f08decf9b4d32079
louiswalsh <louievw5@gmail.com>
2021-02-02 20:26:19 -0500

    Create flask application that currently returns two lists of ratings arrays. Create function that does not require going into db for the logic

--- blacktop_server/app.py
+++ blacktop_server/app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
+import serialize_from_excel as sfe
+
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 #Temporary home page
-@app.route('/')
+@app.route('/teams')
 @cross_origin(supports_credentials=True)
-def hello():
-    players_count = request.args.get('playersCount')
-    players_rating = request.args.get('playersRating')
-    return 'hi'
+def getTeams():
+    overall_rating = request.args.get('rating', None)
+    players_number  = request.args.get('players', None)
+    print('SUMMARY ' + overall_rating + '>>>>> PLAYERS NUMBER ' + players_number)
+    return sfe.gen_rtg_arrays(int(overall_rating), int(players_number))
+
+    
 #Test home page
 
 
